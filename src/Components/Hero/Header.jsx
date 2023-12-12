6d6a521ddb3b7d0fd0f1a0d108b9c013f780b6fb
agro-pest <sathsara.psf99@gmail.com>
2023-12-12 16:33:48 +0530

    add bar icon to mobile version

--- src/Components/Hero/Header.jsx
+++ src/Components/Hero/Header.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
 import logo from "../../assests/logo.png";
 import { motion } from "framer-motion";
 import { Link } from "react-scroll";
+import Bar from "../../assests/bar.png";
 
 const Header = () => {
+  const [mobile, setMobile] = useState(window.innerWidth <= 480);
+  const [menuOpend, setMenuOpend] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMobile(window.innerWidth <= 480);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const sendMessage = () => {
     let package_name = document.getElementById("package-name").innerText;
     console.log(package_name);
@@ -20,34 +36,92 @@ const Header = () => {
         <img className="header__img" src={logo} alt="logo" />
       </motion.div>
 
-      <ul className="header__menu-items">
-        <li className="header__menu-items__item">
-          <Link to="hero" spy={true} smooth={true} duration={500} offset={50}>
-            Home
-          </Link>
-        </li>
-        <li className="header__menu-items__item">
-          <Link to="services" spy={true} smooth={true} duration={500}>
-            Services
-          </Link>
-        </li>
+      {menuOpend === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--seccondPrimaryColor)",
+            width: "4vw",
+            display: "flex",
+            justifyContent: "center",
+            padding: "2vw",
+            borderRadius: "1vw",
+            position: "fixed",
+            left: "90vw",
+            top: "12px",
+            zIndex: "99",
+          }}
+        >
+          <img
+            src={Bar}
+            onClick={() => setMenuOpend(true)}
+            style={{
+              width: "6vw",
+              height: "6vw",
+            }}
+          />
+        </div>
+      ) : (
+        <ul className="header__menu-items">
+          <li className="header__menu-items__item">
+            <Link
+              to="hero"
+              spy={true}
+              smooth={true}
+              duration={500}
+              offset={50}
+              onClick={() => setMenuOpend(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="header__menu-items__item">
+            <Link
+              to="services"
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={() => setMenuOpend(false)}
+            >
+              Services
+            </Link>
+          </li>
 
-        <li className="header__menu-items__item">
-          <Link to="gallery" spy={true} smooth={true} duration={500}>
-            Gallery
-          </Link>
-        </li>
-        <li className="header__menu-items__item">
-          <Link to="packages" spy={true} smooth={true} duration={500}>
-            Packages
-          </Link>
-        </li>
-        <li className="header__menu-items__item">
-          <Link to="contactus" spy={true} smooth={true} duration={500}>
-            Contact us
-          </Link>
-        </li>
-      </ul>
+          <li className="header__menu-items__item">
+            <Link
+              to="gallery"
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={() => setMenuOpend(false)}
+            >
+              Gallery
+            </Link>
+          </li>
+          <li className="header__menu-items__item">
+            <Link
+              to="packages"
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={() => setMenuOpend(false)}
+            >
+              Packages
+            </Link>
+          </li>
+          <li className="header__menu-items__item">
+            <Link
+              to="contactus"
+              spy={true}
+              smooth={true}
+              duration={500}
+              onClick={() => setMenuOpend(false)}
+            >
+              Contact us
+            </Link>
+          </li>
+        </ul>
+      )}
+
       <motion.button
         className="header__button"
         whileHover={{
